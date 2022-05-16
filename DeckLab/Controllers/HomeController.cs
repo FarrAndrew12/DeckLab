3e19416c99773f58a8c715ef24bd5d92de30dcec
FarrAndrew12 <101841142+FarrAndrew12@users.noreply.github.com>
2022-05-16 17:29:28 -0400

    first commit

---
+++ DeckLab/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using DeckLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeckLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public DeckAI dal = new DeckAI();
+       
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Deck cards = dal.CallAPI("ztnd3y2kis02", "draw");
+            return View(cards);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DeckLab/Models/DeckAI.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace DeckLab.Models
+{
+    public class DeckAI
+    {
+        public Deck CallAPI(string id, string endpoint)
+        {
+            string url = $"https://deckofcardsapi.com/api/deck/{id}/{endpoint}/?count=5";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string JSON = rd.ReadToEnd();
+            Deck result = JsonConvert.DeserializeObject<Deck>(JSON);
+            return result;
+        }
+        //    public List<Card> GetCards(string JSON)
+        //    {
+        //        List<Card> result = JsonConvert.DeserializeObject<List<Card>>(JSON);
+        //        return result;
+        //    }
+    }
+}    
+//https://deckofcardsapi.com/api/deck/ztnd3y2kis02/draw/?count=5
+//lz3s327dp705
